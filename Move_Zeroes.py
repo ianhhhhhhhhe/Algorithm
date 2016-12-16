284f48c94688259b3b4f70a76fc9db6ecce20cd5
IanPeverell <517382432@qq.com>
2016-12-16 13:29:23 +0800

    Move_Zeroes

---
+++ Move_Zeroes.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            if not nums[-i-1]:
+                del(nums[-i-1])
+                nums.append(0)
