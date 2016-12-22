79271c9ca6aac9137e1f5cfcaedea61f0bb47be5
IanPeverell <517382432@qq.com>
2016-12-22 12:54:07 +0800

    Power of Three

---
+++ Power_of_Three.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if not n:
+            return False
+        if n == 1:
+            return True
+        while n:
+            if n % 3 and n != 1:
+                return False
+            n /= 3
+        return True
+
+class Solution2(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        p3 = [1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147, 531441, 1594323, 4782969, 14348907, 43046721, 129140163, 387420489, 1162261467, 3486784401]
+        for i in range(len(p3)):
+            if p3[i] == n:
+                return True
+        return False
